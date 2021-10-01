e790b854d2061fb6c972df77513f3f6efcd17c0c
Giruba ぎるば <itsmemariomario123@gmail.com>
2021-10-01 23:35:24 +0800

    Initial Commit

---
+++ app/src/main/java/com/zhekai/universityvenuemanagementsystem/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.zhekai.universityvenuemanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+}
